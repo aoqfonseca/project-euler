bab5a9ecf4b6543cc894d7a3a40b0e0ed184c17e
Andre Fonseca <aoqfonseca@gmail.com>
2011-09-26 07:27:23 -0300

    comecei a resolver o problema 11

--- problem10.py
+++ problem10.py
@@ -1,6 +1,6 @@
-from util import verifica_se_eh_primo 
+from util import verifica_se_eh_primo
 limit = 2000000
-sum = 0 
+sum = 0
 for i in range(2, limit):
     if verifica_se_eh_primo(i):
         sum = sum + i
